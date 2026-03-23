b0102175d97704a235a6ba3824544e14776bd2ea
Khalos CF Moscato <khalos.moscato@gmail.com>
2026-03-23 15:07:46 +0000

    feat: task 7 -> add StringManipulator class with 2 methods and respective test file, run tests

---
+++ TDDLearning.Core/StringManipulator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TDDLearning.Core
+{
+    public class StringManipulator
+    {
+        public string ReverseString(string input)
+        {
+            if (string.IsNullOrEmpty(input)) return input;
+            return new string(input.Reverse().ToArray());
+        }
+
+        public bool IsPalindrome(string input)
+        {
+            if (string.IsNullOrEmpty(input)) return true;
+            string cleanInput = input.ToLower();
+            return ReverseString(input) == cleanInput;
+        }
+    }
+}
---
+++ TDDLearning.Tests/StringManipulatorTests.cs
@@ -0,0 +1,49 @@
+﻿using TDDLearning.Core;
+using StringManipulator = TDDLearning.Core.StringManipulator;
+
+
+namespace TDDLearning.Tests;
+
+public class StringManipulatorTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    [TestCase(Description = "Prints reversed string")]
+    public void ReverseString_ReturnsReversedString()
+    {
+        string string1 = "";
+        string string2 = "racecar";
+        string string3 = "food";
+        StringManipulator manipulator = new StringManipulator();
+
+       
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(manipulator.ReverseString(string1), Is.EqualTo(""));
+            Assert.That(manipulator.ReverseString(string2), Is.EqualTo("racecar"));
+            Assert.That(manipulator.ReverseString(string3), Is.EqualTo("doof"));
+        });
+    }
+
+    [Test]
+    [TestCase(Description = "Check if input string is a palindromw")]
+    public void IsPalindrome_ReturnsTrue()
+    {
+        string string1 = "";
+        string string2 = "racecar";
+        StringManipulator manipulator = new StringManipulator();
+
+
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(manipulator.IsPalindrome(string1), Is.EqualTo(true));
+            Assert.That(manipulator.IsPalindrome(string2), Is.EqualTo(true));
+        });
+    }
+}
