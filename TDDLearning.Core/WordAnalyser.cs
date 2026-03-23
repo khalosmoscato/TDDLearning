1a8d369b39376f5d9bfbb51ad2e5a4562e97f278
Khalos CF Moscato <khalos.moscato@gmail.com>
2026-03-23 21:29:19 +0000

    feat: task 8 -> add WordAnalyser class and test unit, run tests

---
+++ TDDLearning.Core/WordAnalyser.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace TDDLearning.Core
+{
+    public class WordAnalyser
+    {
+        public List<string> FindLongestWords(string text)
+        {
+            List<string> longestWords = new List<string>();
+            if (string.IsNullOrWhiteSpace(text)) return longestWords;
+
+            string cleanText = Regex.Replace(text, @"[^\w\s]", "");
+            string[] words = cleanText.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            int count = 0;
+            foreach (string word in words) 
+            {
+                if (word.Length > count) 
+                { 
+                    count = word.Length;
+                    longestWords.Clear();
+                    longestWords.Add(word); 
+                }
+                else if (word.Length == count) { longestWords.Add(word); }
+            }
+            return longestWords;
+        }
+
+        public Dictionary<char, int> CalculateLetterFrequency(string text)
+        {
+            Dictionary<char, int> charFrequency = new Dictionary<char, int>();
+            if (string.IsNullOrWhiteSpace(text)) return charFrequency;
+
+            string cleanText = text.ToLower();
+
+            foreach (char c in cleanText)
+            {
+                if (char.IsLetter(c))
+                {
+                    if (charFrequency.ContainsKey(c))
+                    {
+                        charFrequency[c]++;
+                    }
+                    else
+                    {
+                        charFrequency.Add(c, 1);
+                    }
+                }
+            }
+            return charFrequency;
+        }
+    }
+}
---
+++ TDDLearning.Tests/WordAnalyserTests.cs
@@ -0,0 +1,56 @@
+﻿using TDDLearning.Core;
+
+namespace TDDLearning.Tests;
+
+[TestFixture]
+public class WordAnalyserTests
+{
+    private WordAnalyser _analyser;
+
+    [SetUp]
+    public void Setup()
+    {
+        _analyser = new WordAnalyser();
+    }
+
+    [Test]
+    [TestCase(Description = "It finds the longest word in a simple sentence")]
+    public void FindLongestWords_SimpleSentence_ReturnsLongestWord()
+    {
+        string input = "This is a fairly boring sentence.";
+        var result = _analyser.FindLongestWords(input);
+        Assert.That(result, Contains.Item("sentence"));
+    }
+
+    [Test]
+    [TestCase(Description = "It finds multiple longest words that share same length")]
+    public void FindLongestWords_SameLengthLongestWords_ReturnsLongestWords()
+    {
+        string input = "This is a fairly boring thing.";
+        var result = _analyser.FindLongestWords(input);
+        Assert.Multiple(() =>
+        {
+            Assert.That(result, Contains.Item("fairly"));
+            Assert.That(result, Contains.Item("boring"));
+        });
+    }
+
+    [Test]
+    [TestCase(Description = "It returns 3 if asked how many `l` in `Hello world`")]
+    public void CalculateLetterFrequency_HelloWorld_l_Returns3()
+    {
+        string input = "Hello world";
+        var result = _analyser.CalculateLetterFrequency(input);
+        Assert.That(result['l'], Is.EqualTo(3));
+    }
+
+    [Test]
+    [TestCase(Description = "It returns 0 if trying to get a letter not in the string")]
+    public void CalculateLetterFrequency_GetLetterNotInString_Returns0()
+    {
+        string input = "This is a fairly boring thing.";
+        var result = _analyser.CalculateLetterFrequency(input);
+        int zInInput = result.GetValueOrDefault('z');
+        Assert.That(zInInput, Is.EqualTo(0));
+    }
+}
