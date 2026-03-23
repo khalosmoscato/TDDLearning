de24cc86eb6d533f343babfe00d7ceacd3a25d40
Khalos CF Moscato <khalos.moscato@gmail.com>
2026-03-23 12:15:49 +0000

    feat: task 3 -> add CompassTests and tested Rotate()

---
+++ TDDLearning.Tests/CompassTests.cs
@@ -0,0 +1,24 @@
+﻿using System.Drawing;
+using TDDLearning.Core;
+using Point = TDDLearning.Core.Point;
+
+namespace TDDLearning.Tests;
+
+public class CompassTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Rotate_NorthAndRight_ReturnsEast()
+    {
+        Compass compass = new Compass(Point.North);
+        Point startPoint = Point.North;
+        Direction direction = Direction.Right;
+
+        Point result = compass.Rotate(startPoint, direction);
+        Assert.That(result, Is.EqualTo(Point.East));
+    }
+}
