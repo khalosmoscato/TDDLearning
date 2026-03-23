add40d36e701fe0eff38b97c33a73de33b2b7a00
Khalos CF Moscato <khalos.moscato@gmail.com>
2026-03-23 11:32:12 +0000

    Add project files.

---
+++ TDDLearning.Core/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TDDLearning.Core
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
